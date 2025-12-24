d73df3007ed7b24540248b54fa5f859cdc076a45
emilianooferreyra <emilianooferreyra@gmail.com>
2025-12-23 22:39:29 -0300

    setup project

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+
+	controller "github.com/emilianooferreyra/MagicStreamMoviesServer/controllers"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.Default()
+
+	router.GET("/movies", controller.GetMovies())
+
+	if err := router.Run(":8080"); err != nil {
+		fmt.Println("Fail to start server", err)
+	}
+}
